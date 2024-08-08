3400fdf8b849e3c41e2ec31b5d1b2ac3774434e8
Liveplay01 <83013750+Liveplay01@users.noreply.github.com>
2024-08-08 21:53:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["/join","GAYmes"];
+const statusMessages = ["/join | GAYmes","with some toys"];
 
 
 let currentIndex = 0;
@@ -74,7 +74,7 @@ function updateStatusAndSendMessages() {
 
   client.user.setPresence({
     activities: [{ name: currentStatus, type: ActivityType.Custom}],
-    status: 'dnd',
+    status: 'playing',
   });
 
   
